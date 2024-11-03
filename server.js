5bba794fd3e40021586bb31e78d212e1e8a84ba9
PetrukKO <drimlyuga.com>
2024-11-03 13:07:17 +0200

    create server via http

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const dotenv = require("dotenv");
 const app = require("./src/index.js");
+const http = require("http");
 const mongoose = require("mongoose");
 dotenv.config({ path: "config.env" });
 const PORT = process.env.PORT || 3000;
@@ -8,12 +9,13 @@ mongoose.connection.on("open", () => {
   console.log("handshake established");
 });
 mongoose.connection.on("error", (err) => {
-  console.error(err);
+  console.error("DB error");
 });
 
-app.listen(PORT, () => {
+const server = http.createServer(app);
+server.listen(PORT, () => {
   console.log(`listening ${PORT}`);
   mongoose
     .connect(process.env.CONNECTION_STRING)
-    .catch((err) => console.log("connection error"));
+    .catch((err) => console.log("DB connection error"));
 });
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/authRoutes");
 const roomRoutes = require("./routes/roomRoutes");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 app.use(express.json());
 
 app.use("/users", userRoutes);
